212bac99aad4423d46caec0799ff9c6989fb3246
carrielz <pedro.carriel@saveincloud.com>
2026-04-20 17:32:00 -0300

    fix: fixed json reading order

--- events.py
+++ events.py
@@ -3,7 +3,7 @@ import docker
 client = docker.DockerClient(base_url="unix://var/run/docker.sock")
 
 for event in client.events(decode=True, filters={"event": "die"}):
-    container_id = event["id"]
+    container_id = event["Actor"]["ID"]
     container_name = event["Actor"]["Attributes"]["name"]
     epoch_time = event["time"]
     print ("O container %s (%s) foi finalizado às %s" % (container_name, container_id, epoch_time))
