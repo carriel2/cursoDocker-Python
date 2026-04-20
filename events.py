70008e46f098f12ded89afc30a5c22e7654d0b86
carrielz <pedro.carriel@saveincloud.com>
2026-04-20 17:27:21 -0300

    feat: added formatted response and print for events

--- events.py
+++ events.py
@@ -3,4 +3,7 @@ import docker
 client = docker.DockerClient(base_url="unix://var/run/docker.sock")
 
 for event in client.events(decode=True, filters={"event": "die"}):
-    print(event)
+    container_id = event["id"]
+    container_name = event["Actor"]["Attributes"]["name"]
+    epoch_time = event["time"]
+    print ("O container %s (%s) foi finalizado às %s" % (container_name, container_id, epoch_time))
