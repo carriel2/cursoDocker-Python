7c828feea21d279872d5880409388f73dac5b5d1
carrielz <pedro.carriel@saveincloud.com>
2026-04-20 17:18:58 -0300

    feat: removed tcp use and added filters to socket formatation

--- events.py
+++ events.py
@@ -2,5 +2,5 @@ import docker
 
 client = docker.DockerClient(base_url="unix://var/run/docker.sock")
 
-for event in client.events(decode=True, filter={"event": "die"}):
+for event in client.events(decode=True, filters={"event": "die"}):
     print(event)
