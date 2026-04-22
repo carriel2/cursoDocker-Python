b1ed86fa3a5d936fb177415d5e82079191ef64d7
carrielz <pedro.carriel@saveincloud.com>
2026-04-22 08:37:33 -0300

    feat: added new requirements.txt and integration w/ discord

--- events.py
+++ events.py
@@ -1,11 +1,17 @@
 import docker
 import datetime
+import requests
 
 client = docker.DockerClient(base_url="unix://var/run/docker.sock")
+webhook_url = "https://discord.com/api/webhooks/1496473445020602539/88NuoFcHJXB38N01OoCVdMb-cky3CYTD42aw20jw0R6FSDJayfc6L8NjQRZnXLF_myp_"
 
 for event in client.events(decode=True, filters={"event": "die"}):
     container_id = event["Actor"]["ID"]
     container_name = event["Actor"]["Attributes"]["name"]
     epoch_time = event["time"]
     date_time  = datetime.datetime.fromtimestamp(epoch_time)
-    print ("O container %s (%s) foi finalizado às %s" % (container_name, container_id, date_time))
+    
+    payload = {"content": "Informação: O container %s (%s) foi finalizado às %s" % (container_name, container_id, date_time)}
+    
+    print (payload)
+    requests.post(webhook_url, data=payload)
